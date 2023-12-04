50735da13716fa0ab35849504231440f51e6507f
Tony Pujals <tpujals@gmail.com>
2023-12-03 18:35:25 -0800

    chore: latest build

---
+++ types/app.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="node" resolution-mode="require"/>
+import { Server } from "node:http";
+export declare const app: import("express-serve-static-core").Express;
+export declare function start(port: number | string): Promise<Server>;
+export declare function stop(signal?: string | Error): Promise<void>;
+//# sourceMappingURL=app.d.ts.map
